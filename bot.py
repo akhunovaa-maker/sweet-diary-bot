70de4b73b9e3e42623e6adcb1ff2dc9eed8046c3
akhunovaa-maker <akhunova_a@flowwow.com>
2026-05-28 16:41:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def q2(update, context):
         await update.message.reply_text("Напиши что делаешь 👇", reply_markup=ReplyKeyboardMarkup([[]], resize_keyboard=True))
         return Q2_OTHER
     context.user_data["q2"] = update.message.text
-    keyboard = [["🧍 Один(а)", "👔 С коллегами"], ["👫 С друзьями", "👨‍👩‍👧 С семьёй"], ["✏️ Другое"]]
+    keyboard = [["🧍 Один", "👔 С коллегами"], ["👫 С друзьями", "👨‍👩‍👧 С семьёй"], ["✏️ Другое"]]
     await update.message.reply_text(
         "С кем ты?",
         reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
